cc78656829d3aabe44c657c2a5210b00c2f8a8df
Michael Rhodes <mike.rhodes@gmail.com>
2013-12-10 14:45:13 +0000

    Add a copyright notice update for a change I’d missed that I’d made.
    The change was changing userAgentHeader to use a CloudantSync user
    agent.

--- Classes/common/touchdb/TDRemoteRequest.m
+++ Classes/common/touchdb/TDRemoteRequest.m
@@ -5,6 +5,10 @@
 //  Created by Jens Alfke on 12/15/11.
 //  Copyright (c) 2011 Couchbase, Inc. All rights reserved.
 //
+//  Modified by Michael Rhodes, 2013
+//  Copyright (c) 2013 Cloudant, Inc. All rights reserved.
+//
+//
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
 //  except in compliance with the License. You may obtain a copy of the License at
 //    http://www.apache.org/licenses/LICENSE-2.0
