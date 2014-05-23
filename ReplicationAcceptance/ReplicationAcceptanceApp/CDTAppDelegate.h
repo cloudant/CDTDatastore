0397d17c186401b86ee66584c48aa056d438bce7
Michael Rhodes <mike.rhodes@gmail.com>
2014-05-23 10:38:07 +0100

    Allow ReplicationAcceptance tests to be run on devices
    By adding an app to the project.

---
+++ ReplicationAcceptance/ReplicationAcceptanceApp/CDTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CDTAppDelegate.h
+//  ReplicationAcceptanceApp
+//
+//  Created by Michael Rhodes on 21/05/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
