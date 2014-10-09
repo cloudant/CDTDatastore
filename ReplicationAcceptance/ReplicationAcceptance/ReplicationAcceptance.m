9c8ec4340fb5a95e14a6026a88b3c925d1419246
Tom Blench <tom.blench@cloudant.com>
2014-10-09 14:55:27 +0100

    Remove unused variable 'count'

--- ReplicationAcceptance/ReplicationAcceptance/ReplicationAcceptance.m
+++ ReplicationAcceptance/ReplicationAcceptance/ReplicationAcceptance.m
@@ -949,7 +949,6 @@ static NSUInteger largeRevTreeSize = 1500;
                         [NSString stringWithFormat:@"doc-%i", 23]];
 
     [self pullFromRemoteWithClientFilterDocIds:filterDocIds];
-    int count = [self.datastore documentCount];
     STAssertTrue(filterDocIds.count == [self.datastore documentCount], @"unexpected number of docs: %@",[self.datastore documentCount]);
     
     NSArray *localDocs = [self.datastore getDocumentsWithIds:filterDocIds];
