4af1c1887803b895b1148219319f8b2cb1e8b30d
Mike Kistler <mkistler@us.ibm.com>
2015-03-08 11:47:41 -0500

    XCTest for CoreData batch update request (Issue #30).

--- Tests/Tests/IncrementalStore.m
+++ Tests/Tests/IncrementalStore.m
@@ -990,6 +990,77 @@ static void *ISContextProgress = &ISContextProgress;
     NSLog(@"Success");
 }
 
+- (void)testBatchUpdates
+{
+    int num_entries = 100;
+
+    XCTAssertTrue((num_entries % 4) == 0, @"Test assumes num_entries is mod 4: %d", num_entries);
+
+    NSError *err = nil;
+    // This will create the database
+    NSManagedObjectContext *moc = self.managedObjectContext;
+    XCTAssertNotNil(moc, @"could not create Context");
+
+    for (int i = 0; i < num_entries; i++) {
+        Entry *e = MakeEntry(moc);
+        e.text = NSStringFromSelector(_cmd);
+        e.check = (i % 2) ? @NO : @YES;
+        e.i64 = @(i);
+    }
+
+    // push it out
+    XCTAssertTrue([moc save:&err], @"Save Failed: %@", err);
+
+    NSArray *results;
+    /**
+     *  Fetch checked entries
+     */
+    NSPredicate *checked = [NSPredicate predicateWithFormat:@"check == YES"];
+
+    NSFetchRequest *fr = [NSFetchRequest fetchRequestWithEntityName:@"Entry"];
+    fr.shouldRefreshRefetchedObjects = YES;
+    fr.predicate = checked;
+
+    err = nil;
+    results = [moc executeFetchRequest:fr error:&err];
+    XCTAssertNotNil(results, @"Expected results: %@", err);
+
+    XCTAssertTrue([results count] == (num_entries / 2), @"results count should be %d is %lu",
+                  num_entries / 2, (unsigned long)[results count]);
+
+    for (Entry *e in results) {
+        XCTAssertTrue([e.check boolValue], @"not even?");
+
+        long long val = [e.i64 longLongValue];
+        XCTAssertTrue((val % 2) == 0, @"entry.i64 should be even");
+    }
+
+    /**
+     *  Batch update all objects 50 or higher (should add 25 checks)
+     */
+    NSBatchUpdateRequest *req = [[NSBatchUpdateRequest alloc] initWithEntityName:@"Entry"];
+    req.predicate = [NSPredicate predicateWithFormat:@"i64>=%d", num_entries / 2];
+    req.propertiesToUpdate = @{ @"check" : @(YES) };
+    req.resultType = NSUpdatedObjectsCountResultType;
+    NSBatchUpdateResult *res = (NSBatchUpdateResult *)[moc executeRequest:req error:&err];
+    XCTAssertNotNil(res, @"Expected results: %@", err);
+    NSLog(@"%@ objects updated", res.result);
+
+    XCTAssertTrue([res.result integerValue] == (3 * num_entries / 4),
+                  @"results count should be %d is %ld", 3 * num_entries / 4,
+                  (long)[res.result integerValue]);
+
+    /**
+     *  Fetch checked entries
+     */
+    fr.predicate = checked;
+    results = [moc executeFetchRequest:fr error:&err];
+    XCTAssertNotNil(results, @"Expected results: %@", err);
+
+    XCTAssertTrue([results count] == ((3 * num_entries) / 4), @"results count should be %d is %lu",
+                  (3 * num_entries) / 4, (unsigned long)[results count]);
+}
+
 - (void)testCheckNumbers
 {
     NSError *err = nil;
