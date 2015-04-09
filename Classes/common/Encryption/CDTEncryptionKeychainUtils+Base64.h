423b3a9b0460136ea442ce6df93a783a144c99cc
Enrique de la Torre Fernandez <enriqued@uk.ibm.com>
2015-04-09 15:54:33 +0100

    Extract Base64 conversion code in Utils to a category.
    After adding some tests, we will convert this code to use Apple security
    modules.

---
+++ Classes/common/Encryption/CDTEncryptionKeychainUtils+Base64.h
@@ -0,0 +1,25 @@
+//
+//  CDTEncryptionKeychainUtils+Base64.h
+//  
+//
+//  Created by Enrique de la Torre Fernandez on 09/04/2015.
+//
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+//
+
+#import "CDTEncryptionKeychainUtils.h"
+
+@interface CDTEncryptionKeychainUtils (Base64)
+
++ (NSString *)base64StringFromData:(NSData *)data length:(int)length isSafeUrl:(bool)isSafeUrl;
++ (NSData *)base64DataFromString:(NSString *)string;
++ (BOOL)isBase64Encoded:(NSString *)str;
+
+@end
---
+++ Classes/common/Encryption/CDTEncryptionKeychainUtils+Base64.m
@@ -0,0 +1,215 @@
+//
+//  CDTEncryptionKeychainUtils+Base64.m
+//  
+//
+//  Created by Enrique de la Torre Fernandez on 09/04/2015.
+//
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+//
+
+#import "CDTEncryptionKeychainUtils+Base64.h"
+
+#import "CDTEncryptionKeychainConstants.h"
+
+const static char base64EncodingTable[64] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+
+const static char base64EncodingTableUrlSafe[64] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'};
+
+@implementation CDTEncryptionKeychainUtils (Base64)
+
+#pragma mark - Public class methods
++ (NSString *)base64StringFromData:(NSData *)data length:(int)length isSafeUrl:(bool)isSafeUrl
+{
+    unsigned long ixtext, lentext;
+    long ctremaining;
+    unsigned char input[3], output[4];
+    short i, charsonline = 0, ctcopy;
+    const unsigned char *raw;
+    NSMutableString *result;
+    
+    lentext = [data length];
+    if (lentext < 1) {
+        return @"";
+    }
+    
+    result = [NSMutableString stringWithCapacity:lentext];
+    raw = [data bytes];
+    ixtext = 0;
+    
+    while (true) {
+        ctremaining = lentext - ixtext;
+        if (ctremaining <= 0) {
+            break;
+        }
+        
+        for (i = 0; i < 3; i++) {
+            unsigned long ix = ixtext + i;
+            if (ix < lentext) {
+                input[i] = raw[ix];
+            } else {
+                input[i] = 0;
+            }
+        }
+        output[0] = (input[0] & 0xFC) >> 2;
+        output[1] = ((input[0] & 0x03) << 4) | ((input[1] & 0xF0) >> 4);
+        output[2] = ((input[1] & 0x0F) << 2) | ((input[2] & 0xC0) >> 6);
+        output[3] = input[2] & 0x3F;
+        
+        ctcopy = 4;
+        switch (ctremaining) {
+            case 1: {
+                ctcopy = 2;
+                break;
+            }
+            case 2: {
+                ctcopy = 3;
+                break;
+            }
+        }
+        for (i = 0; i < ctcopy; i++) {
+            [result
+             appendString:[NSString
+                           stringWithFormat:@"%c", isSafeUrl
+                           ? base64EncodingTableUrlSafe[output[i]]
+                           : base64EncodingTable[output[i]]]];
+        }
+        for (i = ctcopy; i < 4; i++) {
+            [result appendString:@"="];
+        }
+        
+        ixtext += 3;
+        charsonline += 4;
+        
+        if ((length > 0) && (charsonline >= length)) {
+            charsonline = 0;
+        }
+    }
+    
+    return result;
+}
+
++ (NSData *)base64DataFromString:(NSString *)string
+{
+    unsigned long ixtext, lentext;
+    unsigned char ch;
+    unsigned char inbuf[4] = {};
+    unsigned char outbuf[3];
+    short i, ixinbuf;
+    Boolean flignore, flendtext = false;
+    const unsigned char *tempcstring;
+    NSMutableData *theData;
+    
+    if (string == nil) {
+        return [NSData data];
+    }
+    
+    ixtext = 0;
+    
+    tempcstring = (const unsigned char *)[string UTF8String];
+    
+    lentext = [string length];
+    
+    theData = [NSMutableData dataWithCapacity:lentext];
+    
+    ixinbuf = 0;
+    
+    while (true) {
+        if (ixtext >= lentext) {
+            break;
+        }
+        
+        ch = tempcstring[ixtext++];
+        
+        flignore = false;
+        
+        if ((ch >= 'A') && (ch <= 'Z')) {
+            ch = ch - 'A';
+        } else if ((ch >= 'a') && (ch <= 'z')) {
+            ch = ch - 'a' + 26;
+        } else if ((ch >= '0') && (ch <= '9')) {
+            ch = ch - '0' + 52;
+        } else if (ch == '+') {
+            ch = 62;
+        } else if (ch == '=') {
+            flendtext = true;
+        } else if (ch == '/') {
+            ch = 63;
+        } else {
+            flignore = true;
+        }
+        
+        if (!flignore) {
+            short ctcharsinbuf = 3;
+            Boolean flbreak = false;
+            
+            if (flendtext) {
+                if (ixinbuf == 0) {
+                    break;
+                }
+                
+                if ((ixinbuf == 1) || (ixinbuf == 2)) {
+                    ctcharsinbuf = 1;
+                } else {
+                    ctcharsinbuf = 2;
+                }
+                
+                ixinbuf = 3;
+                
+                flbreak = true;
+            }
+            
+            inbuf[ixinbuf++] = ch;
+            
+            if (ixinbuf == 4) {
+                ixinbuf = 0;
+                
+                outbuf[0] = (inbuf[0] << 2) | ((inbuf[1] & 0x30) >> 4);
+                outbuf[1] = ((inbuf[1] & 0x0F) << 4) | ((inbuf[2] & 0x3C) >> 2);
+                outbuf[2] = ((inbuf[2] & 0x03) << 6) | (inbuf[3] & 0x3F);
+                
+                for (i = 0; i < ctcharsinbuf; i++) {
+                    [theData appendBytes:&outbuf[i] length:1];
+                }
+            }
+            
+            if (flbreak) {
+                break;
+            }
+        }
+    }
+    
+    return theData;
+}
+
++ (BOOL)isBase64Encoded:(NSString *)str
+{
+    NSString *pattern =
+    [[NSString alloc] initWithFormat:CDTENCRYPTION_KEYCHAIN_BASE64_REGEX, [str length]];
+    
+    NSError *error = NULL;
+    NSRegularExpression *regex =
+    [NSRegularExpression regularExpressionWithPattern:pattern options:0 error:&error];
+    NSUInteger numMatch =
+    [regex numberOfMatchesInString:str options:0 range:NSMakeRange(0, [str length])];
+    if (numMatch != 1 || [error code] != 0) {
+        return NO;
+    }
+    return YES;
+}
+
+@end
