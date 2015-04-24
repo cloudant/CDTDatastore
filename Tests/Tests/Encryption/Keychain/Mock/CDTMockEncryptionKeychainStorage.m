297d768528751e431f5a654d76efbc3cc13bed81
Enrique de la Torre Fernandez <enriqued@uk.ibm.com>
2015-04-24 13:54:55 +0100

    Add manager to generate & retrieve a key
    A manager to generate & retrieve a key based on the
    classes implemented so far.

---
+++ Tests/Tests/Encryption/Keychain/Mock/CDTMockEncryptionKeychainStorage.m
@@ -0,0 +1,75 @@
+//
+//  CDTMockEncryptionKeychainStorage.m
+//  Tests
+//
+//  Created by Enrique de la Torre Fernandez on 15/04/2015.
+//  Copyright (c) 2015 IBM Cloudant. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+//
+
+#import "CDTMockEncryptionKeychainStorage.h"
+
+@interface CDTMockEncryptionKeychainStorage ()
+
+@end
+
+@implementation CDTMockEncryptionKeychainStorage
+
+#pragma mark - Init object
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _encryptionKeyDataExecuted = NO;
+        _encryptionKeyDataResult = CDTMOCKENCRYPTIONKEYCHAINSTORAGE_DEFAULT_DATA;
+        
+        _saveEncryptionKeyDataExecuted = NO;
+        _saveEncryptionKeyDataResult = CDTMOCKENCRYPTIONKEYCHAINSTORAGE_DEFAULT_SAVE;
+        
+        _clearEncryptionKeyDataExecuted = NO;
+        _clearEncryptionKeyDataResult = CDTMOCKENCRYPTIONKEYCHAINSTORAGE_DEFAULT_CLEAR;
+        
+        _encryptionKeyDataExistsExecuted = NO;
+        _encryptionKeyDataExistsResult = CDTMOCKENCRYPTIONKEYCHAINSTORAGE_DEFAULT_ARETHEREDATA;
+    }
+    
+    return self;
+}
+
+#pragma mark - Public methods
+- (CDTEncryptionKeychainData *)encryptionKeyData
+{
+    self.encryptionKeyDataExecuted = YES;
+    
+    return self.encryptionKeyDataResult;
+}
+
+- (BOOL)saveEncryptionKeyData:(CDTEncryptionKeychainData *)data
+{
+    self.saveEncryptionKeyDataExecuted = YES;
+    
+    return self.saveEncryptionKeyDataResult;
+}
+
+- (BOOL)clearEncryptionKeyData
+{
+    self.clearEncryptionKeyDataExecuted = YES;
+    
+    return self.clearEncryptionKeyDataResult;
+}
+
+- (BOOL)encryptionKeyDataExists
+{
+    self.encryptionKeyDataExistsExecuted = YES;
+    
+    return self.encryptionKeyDataExistsResult;
+}
+
+@end
