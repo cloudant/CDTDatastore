30ebaaa1d231b8a692be172dff0c5c81d34b7418
Enrique de la Torre Fernandez <enriqued@uk.ibm.com>
2015-04-27 17:27:43 +0100

    Always 'select sqlite_master' to check if db is readable
    Instead of checking on advance if a db is encrypted, set the key
    (if available) and execute 'select sqlite_master', this command
    will succeed only if:
    - The db is encrypted & the key is correct
    - The db is not encrypted & not key was set

--- Tests/Tests/Encryption/FMDatabase+SQLCipher.m
+++ Tests/Tests/Encryption/FMDatabase+SQLCipher.m
@@ -1,9 +1,17 @@
 //
 //  FMDatabase+SQLCipher.m
-//  
+//  CloudantSync
 //
 //  Created by Enrique de la Torre Fernandez on 29/03/2015.
+//  Copyright (c) 2015 IBM Cloudant. All rights reserved.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
 //
 
 #import "FMDatabase+SQLCipher.h"
@@ -13,28 +21,6 @@
 NSString* const FMDatabaseStandardSQLiteHeader = @"SQLite format 3";
 
 @implementation FMDatabase (SQLCipher)
-
-#pragma mark - Public methods
-- (BOOL)setValidKey:(NSString *)key
-{
-    BOOL result = [self setKey:key];
-    if (!result) {
-        CDTLogError(CDTDATASTORE_LOG_CONTEXT,
-                    @"Key to de-encrypt DB at %@ not set. DB can not be opened.",
-                    [self databasePath]);
-    } else {
-        // Verify if it is the right key
-        result = (sqlite3_exec(self.sqliteHandle, "SELECT count(*) FROM sqlite_master;", NULL, NULL,
-                               NULL) == SQLITE_OK);
-        if (!result) {
-            CDTLogError(CDTDATASTORE_LOG_CONTEXT,
-                        @"DB at %@ can not be deciphered with provided key. DB can not be opened.",
-                        [self databasePath]);
-        }
-    }
-    
-    return result;
-}
 
 #pragma mark - Public class methods
 + (FMDatabaseUnencrypted)isDatabaseUnencryptedAtPath:(NSString *)path
