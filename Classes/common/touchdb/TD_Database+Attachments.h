dbe94333e42d40fe61505a1f60d9b4032637685d
Enrique de la Torre Fernandez <enriqued@uk.ibm.com>
2015-05-05 14:57:58 +0100

    Remove access to attachment file path.
    - Remove method 'TDBlobStore:pathForKey:'
    - Define protocols 'CDTBlobReader' & 'CDTBlobWriter'
    - Implement class 'CDTBlobData'
    - TDBlobStore returns CDTBlobReader instances

--- Classes/common/touchdb/TD_Database+Attachments.h
+++ Classes/common/touchdb/TD_Database+Attachments.h
@@ -9,6 +9,9 @@
 //
 
 #import "TD_Database.h"
+
+#import "CDTBlobReader.h"
+
 @class TDBlobStoreWriter, TDMultipartWriter, FMDatabase;
 
 /** Types of encoding/compression of stored attachments. */
@@ -55,16 +58,16 @@ typedef enum { kTDAttachmentEncodingNone, kTDAttachmentEncodingGZIP } TDAttachme
                             encoding:(TDAttachmentEncoding *)outEncoding
                               status:(TDStatus *)outStatus;
 
-/** Returns the location of an attachment's file in the blob store. */
-- (NSString *)getAttachmentPathForSequence:(SequenceNumber)sequence
-                                     named:(NSString *)filename
-                                      type:(NSString **)outType
-                                  encoding:(TDAttachmentEncoding *)outEncoding
-                                    status:(TDStatus *)outStatus;
+/** Returns the blob for an attachment in the blob store. */
+- (id<CDTBlobReader>)getAttachmentBlobForSequence:(SequenceNumber)sequence
+                                            named:(NSString *)filename
+                                             type:(NSString **)outType
+                                         encoding:(TDAttachmentEncoding *)outEncoding
+                                           status:(TDStatus *)outStatus;
 
 /** Uses the "digest" field of the attachment dict to look up the attachment in the store and return
- * a file URL to it. DO NOT MODIFY THIS FILE! */
-- (NSURL *)fileForAttachmentDict:(NSDictionary *)attachmentDict;
+ * its blob. */
+- (id<CDTBlobReader>)blobForAttachmentDict:(NSDictionary *)attachmentDict;
 
 /** Deletes obsolete attachments from the database and blob store. */
 - (TDStatus)garbageCollectAttachments:(FMDatabase *)db;
