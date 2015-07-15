fc12a7d3d5df8c30e68108584027b09115f100a8
Rhys Short <rhyshort@uk.ibm.com>
2015-07-15 11:42:22 +0100

    Correct init chain for CDTMutableDocumentRevision
    Correct the init chain for CDTMutableDocumentRevision so each init method
    calls down into the full argument version of init.
    
    Change document body and attachments to initalize with a empty dictionary
    if nil is passed for their arguments.

--- Classes/common/CDTMutableDocumentRevision.h
+++ Classes/common/CDTMutableDocumentRevision.h
@@ -13,11 +13,39 @@
 @property (nonatomic, strong, readwrite) NSString *sourceRevId;
 @property (nonatomic, strong, readwrite) NSString *docId;
 
+/**
+ *   Creates an empty CDTMutableDocumentRevision
+ **/
 + (CDTMutableDocumentRevision *)revision;
 
-- (id)initWithDocumentId:(NSString *)documentId body:(NSMutableDictionary *)body;
+/**
+ * Initializes a CDTMutableDocumentRevision revision
+ *
+ * @param documentId The id of the document
+ * @param body The body of the document
+ *
+ **/
+- (instancetype)initWithDocumentId:(NSString *)documentId body:(NSMutableDictionary *)body;
 
-- (id)initWithSourceRevisionId:(NSString *)sourceRevId;
+/**
+ * Initializes a CDTMutableDocumentRevision
+ * 
+ * @param sourceRevId the parent revision id
+ **/
+- (instancetype)initWithSourceRevisionId:(NSString *)sourceRevId;
+
+/**
+ Initializes a CDTMutableDocumentRevision
+ 
+ @param documentId the id of the document
+ @param body the body of the document
+ @param attachments the document's attachments
+ @param sourceRevId the parent revision id
+ **/
+- (instancetype)initWithDocumentId:(NSString*) documentId
+                              body:(NSMutableDictionary *)body
+                       attachments: (NSMutableDictionary *)attachments
+                  sourceRevisionId:(NSString*)sourceRevId;
 
 - (void)setBody:(NSDictionary *)body;
 
