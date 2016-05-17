03a19f5d5e23cd2f647693a4ceacf5d569bcc959
Tom Blench <tom.blench@cloudant.com>
2016-05-17 10:09:52 +0100

    generify for all TEST_* env vars

--- buildplist.sh
+++ buildplist.sh
@@ -5,27 +5,9 @@ info_plist="${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/Replicati
 
 echo $info_plist
 
-if [ "$TEST_COUCH_USERNAME" ]; then
-	echo "Setting couch username"
-    /usr/libexec/PlistBuddy -c "Set :TEST_COUCH_USERNAME '${TEST_COUCH_USERNAME}'" "${info_plist}"
-fi
-
-if [ "$TEST_COUCH_PASSWORD" ]; then
-		echo "Setting couch password"
-/usr/libexec/PlistBuddy -c "Set :TEST_COUCH_PASSWORD '${TEST_COUCH_PASSWORD}'" "${info_plist}"
-fi
-
-if [ "$TEST_COUCH_HOST" ]; then
-		echo "Setting couch host"
-/usr/libexec/PlistBuddy -c "Set :TEST_COUCH_HOST '${TEST_COUCH_HOST}'" "${info_plist}"
-fi
-
-if [ "$TEST_COUCH_PORT" ]; then
-		echo "Setting couch port"
-    /usr/libexec/PlistBuddy -c "Set :TEST_COUCH_PORT '${TEST_COUCH_PORT}'" "${info_plist}";
-fi
-
-if [ "$TEST_COUCH_HTTP" ]; then
-		echo "Setting couch http"
-    /usr/libexec/PlistBuddy -c "Set :TEST_COUCH_HTTP '${TEST_COUCH_HTTP}'" "${info_plist}";
-fi
+for k in ${!TEST_COUCH*}
+do
+    v=${!k}
+    echo "Setting ${k} to ${v}"
+    /usr/libexec/PlistBuddy -c "Set :${k} '${v}'" "${info_plist}"
+done
