b6252bb516091287aade1f6c007db2e786589d60
Rhys Short <rhyshort@uk.ibm.com>
2016-09-02 14:02:33 +0100

    Created category on CDTDatastore to manage replications
    Create category to make it simpler for a user to create a push or pull
    replicator with basic options.
    
    Resolves #267

---
+++ CDTDatastore/CDTReplicator/CDTDatastore+Replication.m
@@ -0,0 +1,53 @@
+//
+// Created by Rhys Short on 02/09/2016.
+// Copyright © 2016 IBM Corporation. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+
+
+#import "CDTDatastore+Replication.h"
+#import "CDTPushReplication.h"
+#import "CDTPullReplication.h"
+#import "CDTReplicator.h"
+
+@interface CDTDatastore ()
+
+@property(readonly) CDTDatastoreManager *manager; // this exists in the CDTDatastoreManager
+@end
+
+@implementation CDTDatastore (Replication)
+
+- (CDTReplicator *)pushReplicationTarget:(NSURL *)target
+                            withDelegate:(NSObject <CDTReplicatorDelegate> *)delegate
+                                   error:(NSError *__autoreleasing *)error {
+    CDTPushReplication *push = [CDTPushReplication replicationWithSource:self target:target];
+    return [self replicatorWithReplication:push delegate:delegate error:error];
+}
+
+- (CDTReplicator *)pullReplicationSource:(NSURL *)source
+                            withDelegate:(NSObject <CDTReplicatorDelegate> *)delegate
+                                   error:(NSError *__autoreleasing *)error {
+
+    CDTPullReplication *pull = [CDTPullReplication replicationWithSource:source target:self];
+    return [self replicatorWithReplication:pull delegate:delegate error:error];
+}
+
+- (CDTReplicator *)replicatorWithReplication:(CDTAbstractReplication *)replication
+                                    delegate:(NSObject <CDTReplicatorDelegate> *)delegate
+                                       error:(NSError *__autoreleasing *)error {
+    CDTReplicator *replicator = [[CDTReplicator alloc] initWithTDDatabaseManager:self.manager.manager
+                                                                     replication:replication
+                                                           sessionConfigDelegate:nil
+                                                                           error:error];
+    replicator.delegate = delegate;
+    return replicator;
+}
+
+
+@end
---
+++ CDTDatastoreTests/DatastoreReplicationTests.m
@@ -0,0 +1,81 @@
+//
+// Created by Rhys Short on 02/09/2016.
+// Copyright (c) 2016 IBM Corporation. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+//  except in compliance with the License. You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software distributed under the
+//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  either express or implied. See the License for the specific language governing permissions
+//  and limitations under the License.
+
+
+#import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
+#import "CloudantSyncTests.h"
+#import "CDTDatastore.h"
+#import "CDTDatastore+Replication.h"
+#import "CDTReplicatorDelegate.h"
+#import "CDTReplicator.h"
+
+@interface ReplicatorDelegate: NSObject<CDTReplicatorDelegate>
+@end
+
+@implementation ReplicatorDelegate
+@end
+
+@interface DatastoreReplicationTests : CloudantSyncTests
+
+@property (nonatomic,strong) CDTDatastore *datastore;
+
+@end
+
+
+@implementation DatastoreReplicationTests
+
+- (void)setUp
+{
+    [super setUp];
+
+    NSError *error;
+    self.datastore = [self.factory datastoreNamed:@"test" error:&error];
+
+    XCTAssertNotNil(self.datastore, @"datastore is nil");
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+
+    self.datastore = nil;
+
+    [super tearDown];
+}
+
+- (void) testPullReplicatorCreatedViaCategory
+{
+    NSError * error = nil;
+    ReplicatorDelegate *delegate = [[ReplicatorDelegate alloc] init];
+    CDTReplicator * replicator = [self.datastore pullReplicationSource:[[NSURL alloc] initWithString:@"http://exmaple.example"]
+                                                          withDelegate:delegate
+                                                                 error:&error];
+    XCTAssertNil(error, "An error should not have been encountered.");
+    XCTAssertNotNil(replicator, "replicator should not be nil");
+    XCTAssertEqual(replicator.delegate, delegate, "the replicator's delegate should be the same as the delegate passed into");
+}
+
+- (void) testPushReplicatorCreatedViaCategory
+{
+    NSError * error = nil;
+    ReplicatorDelegate *delegate = [[ReplicatorDelegate alloc] init];
+    CDTReplicator * replicator = [self.datastore pushReplicationTarget:[[NSURL alloc] initWithString:@"http://example.example"]
+                                                          withDelegate:delegate
+                                                                 error:&error];
+    XCTAssertNil(error, "An error should not have been encountered.");
+    XCTAssertNotNil(replicator, "replicator should not be nil");
+    XCTAssertEqual(replicator.delegate, delegate, "the replicator's delegate should be the same as the delegate passed into");
+}
+
+@end
+
