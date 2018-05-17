ce9d8916888731b4b6a69cf50edcb6c1ab02a138
Tom Blench <tom@blench.org>
2018-05-17 11:34:58 +0100

    Add test to ensure CDTDatastore filehandles aren't leaked (#429)
    * Add test testDatastoreClosesFilehandles
    
    This test ensures that we don't excessively leak filehandles, which is an indication that the
    CDTDatastore and CDTQIndexManager objects are not being dealloc'd correctly
    
    * Free malloc'd memory
    
    * Fix comments, copyright, and exit early if we fail
    
    * Only compile testDatastoreClosesFilehandles on macOS

--- CDTDatastoreTests/DatastoreManagerTests.m
+++ CDTDatastoreTests/DatastoreManagerTests.m
@@ -4,7 +4,7 @@
 //
 //  Created by Rhys Short on 17/12/2014.
 //
-//  Copyright © 2016 IBM Corporation. All rights reserved.
+//  Copyright © 2016, 2018 IBM Corporation. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
 //  except in compliance with the License. You may obtain a copy of the License at
@@ -20,7 +20,11 @@
 #import "TDInternal.h"
 #import "FMDatabase.h"
 #import "FMDatabaseQueue.h"
-
+// for testDatastoreClosesFilehandles
+#if TARGET_OS_OSX
+    #import <libproc.h>
+    #import <sys/proc_info.h>
+#endif
 @interface DatastoreManagerTests : CloudantSyncTests
 
 @end
@@ -95,6 +99,51 @@
         XCTAssertNotNil(lastSequenceJson);
     }
 }
+
+// this can only run on macOS because it uses proc_pidinfo which is not available on iOS
+#if TARGET_OS_OSX
+- (void) testDatastoreClosesFilehandles {
+    // repeatedly obtain the same datastore in a loop, adding documents to it and calling
+    // ensureIndexed on each iteration, check that we have not excessively leaked filehandles, which
+    // would indicate that the datastore and indexmanager are not being dealloc'd correctly
+    int n = 1000;
+    CDTDatastore *ds;
+    for (int i=0; i<n; i++) {
+        @autoreleasepool {
+            CDTDocumentRevision *rev = [CDTDocumentRevision revision];
+            rev.body = [NSMutableDictionary dictionaryWithDictionary: @{@"hello": @"world"}];
+            NSError *err;
+            ds = [self.factory datastoreNamed:@"test" error:&err];
+            XCTAssertNil(err);
+            [ds createDocumentFromRevision:rev error:&err];
+            XCTAssertNil(err);
+            [ds ensureIndexed:@[@"hello"] withName:@"index"];
+            // if the datastore has been properly released, we will not run out of filehandles
+            int pid = [[NSProcessInfo processInfo] processIdentifier];
+            // first we call with a null pointer to see what the minimum buffer size needed is
+            int bufferSize = proc_pidinfo(pid, PROC_PIDLISTFDS, 0, 0, 0);
+            // buffer size is bytes, we want number of processes
+            struct proc_fdinfo *fdInfo = (struct proc_fdinfo *)malloc(bufferSize);
+            int fdCount = bufferSize / PROC_PIDLISTFD_SIZE;
+            // now call with the buffer to get the actual number of processes (may be lower than
+            // buffer size)
+            bufferSize = proc_pidinfo(pid, PROC_PIDLISTFDS, 0, fdInfo, fdCount);
+            free(fdInfo);
+            // buffer size is bytes, we want number of processes
+            fdCount = bufferSize / PROC_PIDLISTFD_SIZE;
+            // as observed in testing, this should never go above 8, but we'll set a conservative
+            // limit of 100 to allow some breathing room
+            bool fdLimitExceeded = fdCount >= 100;
+            XCTAssertTrue(!fdLimitExceeded);
+            if (fdLimitExceeded) {
+                // exit early if we are already above the FD limit
+                return;
+            }
+        }
+    }
+    XCTAssertEqual([ds documentCount], n);
+}
+#endif
 
 @end
 
